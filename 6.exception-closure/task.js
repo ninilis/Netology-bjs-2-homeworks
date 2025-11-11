42ae0593e4ae67063e19a9fea408581f8bd6efe6
ninilis <latifullina1997@mail.ru>
2025-11-11 16:24:20 +0300

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿// Task 1
+function parseCount(units) {
+    const parsedUnits = Number.parseFloat(units);
+    if (isNaN(parsedUnits)) {
+        throw new Error("Невалидное значение");
+    }
+    return parsedUnits;
+}
+
+function validateCount(units) {
+    try {
+        return parseCount(units);
+    } catch (error) {
+        return error;
+    }
+}
+
+// Task 2
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+    get perimeter() {
+        return this.a + this.b + this.c
+    }
+
+    get area() {
+        const halfPerimeter = this.perimeter / 2;
+        const area = Math.sqrt(halfPerimeter * (halfPerimeter - this.a) * (halfPerimeter - this.b) * (halfPerimeter - this.c));
+        return parseFloat(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
